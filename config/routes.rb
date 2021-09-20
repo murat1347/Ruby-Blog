28f3d3aa2c25c2b5dd573156579db55853797e6a
Murat Çiçek <muratcicek@trakya.edu.tr>
2021-09-20 12:40:22 +0300

    edit authenticate system

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcomes/index'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about' =>'pages#about'
-  resources :articles , only:[:show, :new , :index , :create] 
+  resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 end
